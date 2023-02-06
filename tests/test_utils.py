0f5542d7caeee737ee454b3d76d7dba5a62b1df9
aleksey <mc_gra_dy@mail.ru>
2023-02-06 13:00:11 +0300

    исправил сортировку

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,6 +1,12 @@
 from unittest import TestCase
+from utils import print_last_five_operations
 
 
 class Test(TestCase):
     def test_print_last_five_operations(self):
+        self.fail()
+
+
+class Test(TestCase):
+    def test_mask_account(self):
         self.fail()
--- utils.py
+++ utils.py
@@ -4,21 +4,44 @@ import random
 def print_last_five_operations(operations):
     # 5 последних операций
     count = 0
+    executed_operations = []
     for operation in operations:
         if operation['state'] == 'EXECUTED':
-            date = operation['date'].split('T')[0]
-            date = date.split('-')[2] + '.' + date.split('-')[1] + '.' + date.split('-')[0]
-            print(date, operation['description'])
-            if "from" in operation.keys():
-                from_card = operation['from'][:4] + ' ' + operation['from'][4:8] + ' **** ' + operation['from'][-4:]
-            to_account = '**' + operation['to'][-4:]
-            print(from_card, '->', to_account)
-            amount, currency = operation['operationAmount']
-            print(amount, currency)
+            executed_operations.append(operation)
 
-            count += 1
+            executed_operations.sort(key=lambda x: x['date'], reverse=True)
 
-        if count == 5: break
+            for i in range(min(5, len(executed_operations))):
+                operation = executed_operations[i]
+
+                date = operation['date'].split('T')[0]
+                date = date.split('-')[2] + '.' + date.split('-')[1] + '.' + date.split('-')[0]
+                description = operation['description']
+                if "from" in operation.keys():
+                    from_account = mask_account(operation['from'])
+                to_account = mask_account(operation['to'])
+                amount = operation['operationAmount']['amount']
+                currency = operation['operationAmount']['currency']['name']
+
+
+
+        count += 1
+
+        if count == 6: break
+        print('{} {}'.format(date, description))
+        print('{} -> {}'.format(from_account, to_account))
+        print(amount, currency)
+        print()
+def mask_account(number):  # Замаскировать номер счета/карты.
+    if len(number) == 16:  # Visa/Mastercard.
+        return '{} **** {}'.format(' '.join([number[:4], number[8:12]]), number[-4:])
+
+    elif len(number) == 20:  # Maestro.
+        return '{} **** **** {}'.format(' '.join([number[:4], number[8:12]]), number[-4:])
+
+    else:  # Номер счета.
+        return '**{}'.format(number[-4:])
+
 
 def load_operations(filename):
     f = requests.get(filename)
